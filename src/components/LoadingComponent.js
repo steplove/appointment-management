17ca8293c03d612c23a82dd3c98597c99926437b
steplove~ <loveaumak@gmail.com>
2023-11-14 08:29:55 +0700

    ารพ

--- src/components/LoadingComponent.js
+++ src/components/LoadingComponent.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import { Modal, Spinner } from 'react-bootstrap';
+import { Modal } from 'react-bootstrap';
 
 const LoadingComponent = ({ show }) => {
   return (
     <Modal show={show} centered>
       <Modal.Body className="text-center">
-        <Spinner animation="border" role="status">
-          <span className="sr-only">Loading...</span>
-        </Spinner>
-        <p>Loading...</p>
+      <div className="spiner-example">
+        <div className="sk-spinner sk-spinner-three-bounce">
+          <div className="sk-bounce1"></div>
+          <div className="sk-bounce2"></div>
+          <div className="sk-bounce3"></div>
+        </div>
+        <div className="text-center">
+          <p>กำลังค้นหา...</p>
+        </div>
+      </div>
       </Modal.Body>
     </Modal>
   );
