0356e33c3cf2ce09c8b1b37d819816a43a13a3d2
steplove <47933243+steplove@users.noreply.github.com>
2023-12-22 16:38:30 +0700

    ฟหเ

--- src/components/TableBlog.js
+++ src/components/TableBlog.js
@@ -1,22 +1,44 @@
 import React, { useEffect, useState } from "react";
 import { Form, Button, Modal, InputGroup } from "react-bootstrap";
 import ReactPaginate from "react-paginate";
+import Swal from "sweetalert2";
+import { BASE_URL } from "../constants/constants";
+import useFetch from "../hooks/useFetch";
 
 function TableBlog() {
-  const [displayedPackage, setDisplayedPackage] = useState([]);
-
   // กำหนดตัวแปรสำหรับจำนวนข้อมูลที่ต้องการแสดงในแต่ละหน้า
   const [perPage] = useState(10);
+  const { data: blogs = [], refetch } = useFetch(BASE_URL + "/api/blogShow");
+  const [showEdite, setShowEdite] = useState(false);
+  const [selectedBlogs, setSelectedBlogs] = useState(null);
 
-  // สถานะสำหรับการจัดการหน้าปัจจุบันที่แสดง
+  const [Blog_Image, setPackageImage] = useState(null);
+  const [packagePreview, setPackagePreview] = useState(null);
+  const [Blog_ImageBanner, setBannerImage] = useState(null);
+  const [bannerPreview, setBannerPreview] = useState(null);
+  const [blog_Name, setBlog_Name] = useState([]);
+  const [blog_Detail, setBlog_Detail] = useState([]);
+
+  useEffect(() => {
+    if (blogs && blogs.length > 0) {
+      // ตัดข้อมูลที่ต้องการแสดงตามจำนวนข้อมูลในหนึ่งหน้า
+      setDisplayedBlogs(blogs);
+    }
+  }, [blogs]);
+  const [displayedBlogs, setDisplayedBlogs] = useState([]);
+  useEffect(() => {
+    const totalPageCount = Math.ceil(displayedBlogs.length / perPage);
+    setPageCount(totalPageCount);
+  }, [displayedBlogs, perPage]);
   const [currentPage, setCurrentPage] = useState(0);
+  const offset = currentPage * perPage;
+  displayedBlogs.slice(offset, offset + perPage);
+  const [searchResult, setSearchResult] = useState(null);
+  const shouldShowAllData =
+    !searchResult && displayedBlogs && displayedBlogs.length > 0;
 
   // สถานะสำหรับจำนวนหน้าทั้งหมดที่สามารถแสดงได้
   const [pageCount, setPageCount] = useState(0);
-  useEffect(() => {
-    const totalPageCount = Math.ceil(displayedPackage.length / perPage);
-    setPageCount(totalPageCount);
-  }, [displayedPackage, perPage]);
   const handlePageChange = ({ selected }) => {
     setCurrentPage(selected);
   };
@@ -24,6 +46,178 @@ function TableBlog() {
   const handleShow = () => setShow(true);
   const handleClose = () => {
     setShow(false);
+    clearForm();
+  };
+  const handleCloseEdite = () => setShowEdite(false);
+  const handleShowEdite = () => setShowEdite(true);
+
+  const handleEditModal = (Blog_ID) => {
+    const packageToEdit = displayedBlogs.find((p) => p.Blog_ID === Blog_ID);
+    console.log(Blog_ID, "Blog_IDBlog_IDBlog_ID");
+    setSelectedBlogs(packageToEdit);
+    handleShowEdite();
+  };
+  const handleSubmitEdit = async () => {
+    try {
+      const response = await fetch(
+        `${BASE_URL}/api/UpdateBlog/${selectedBlogs.Blog_ID}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            Blog_Name: selectedBlogs.Blog_Name,
+            Blog_Detail: selectedBlogs.Blog_Detail,
+          }),
+        }
+      );
+
+      const data = await response.json();
+      if (data.message === "Blog updated successfully!") {
+        Swal.fire({
+          title: "การอัปเดตสำเร็จ!",
+          icon: "success",
+          showConfirmButton: false,
+          timer: 1500,
+        });
+        refetch();
+        handleCloseEdite(); // ปิด modal
+      } else {
+        Swal.fire({
+          title: "เกิดข้อผิดพลาด!",
+          text: data.message,
+          icon: "error",
+          confirmButtonText: "ตกลง",
+        });
+        handleCloseEdite(); // ปิด modal
+      }
+    } catch (error) {
+      Swal.fire({
+        title: "เกิดข้อผิดพลาด!",
+        text: "ไม่สามารถติดต่อเซิร์ฟเวอร์ได้",
+        icon: "error",
+        confirmButtonText: "ตกลง",
+      });
+      handleCloseEdite(); // ปิด modal
+    }
+  };
+  const handleImageChange = (e, setImageFunction, setPreviewFunction) => {
+    const file = e.target.files[0];
+    if (file) {
+      setImageFunction(file);
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setPreviewFunction(reader.result);
+      };
+      reader.readAsDataURL(file);
+    } else {
+      setImageFunction(null);
+      setPreviewFunction(null);
+    }
+  };
+
+  const handleSave = async () => {
+    try {
+      const formData = new FormData();
+      formData.append("Blog_Name", blog_Name);
+      formData.append("Blog_Detail", blog_Detail);
+
+      if (Blog_Image) {
+        formData.append("Blog_Image", Blog_Image);
+      }
+      if (Blog_ImageBanner) {
+        formData.append("Blog_ImageBanner", Blog_ImageBanner);
+      }
+
+      try {
+        const response = await fetch(`${BASE_URL}/api/blogInsert`, {
+          method: "POST",
+          body: formData,
+        });
+
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+
+        const data = await response.json();
+
+        if (response.ok) {
+          setShow(false);
+          Swal.fire({
+            title: "บันทึกสำเร็จ!",
+            icon: "success",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+        refetch();
+        console.log("Success:", data);
+        clearForm();
+        // Additional logic after a successful API call
+      } catch (error) {
+        setShow(false);
+        Swal.fire({
+          title: "เกิดข้อผิดพลาด!",
+          text: "ไม่สามารถติดต่อกับเซิร์ฟเวอร์",
+          icon: "error",
+        });
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      // Additional error handling logic
+    }
+  };
+  const handleDelete = async (blogID) => {
+    Swal.fire({
+      title: "คุณแน่ใจหรือว่าต้องการลบ?",
+      text: "คุณไม่สามารถย้อนกลับได้หลังจากนี้!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText: "ใช่, ลบเลย!",
+      cancelButtonText: "ยกเลิก",
+      reverseButtons: true,
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        try {
+          const response = await fetch(`${BASE_URL}/api/blogDelete/${blogID}`, {
+            method: "DELETE",
+          });
+          if (response.ok) {
+            Swal.fire({
+              title: "ลบสำเร็จ!",
+              icon: "success",
+              showConfirmButton: false,
+              timer: 1500,
+            });
+            refetch();
+          } else {
+            const data = await response.json();
+            Swal.fire({
+              title: "เกิดข้อผิดพลาด!",
+              text: data.message,
+              icon: "error",
+            });
+          }
+        } catch (error) {
+          Swal.fire({
+            title: "เกิดข้อผิดพลาด!",
+            text: "ไม่สามารถติดต่อกับเซิร์ฟเวอร์",
+            icon: "error",
+          });
+        }
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+        Swal.fire("ยกเลิก", "ข้อมูลของคุณยังคงอยู่", "error");
+      }
+    });
+  };
+  const clearForm = () => {
+    setBlog_Detail("");
+    setBlog_Name("");
+    setPackageImage(null);
+    setPackagePreview(null);
+    setBannerImage(null);
+    setBannerPreview(null);
   };
   return (
     <>
@@ -39,7 +233,7 @@ function TableBlog() {
                     </Button>
                   </InputGroup>
                 </div>
-                <div className="ml-auto col-sm-2">
+                {/* <div className="ml-auto col-sm-2">
                   <Form.Group controlId="searchFirstName">
                     <Form.Label>ชื่อแพ็คเกจ</Form.Label>
                     <Form.Control
@@ -48,8 +242,8 @@ function TableBlog() {
                       //   onChange={(e) => setUsersCode(e.target.value)}
                     />
                   </Form.Group>
-                </div>
-                <div style={{ marginTop: "25px", marginLeft: "20px" }}>
+                </div> */}
+                {/* <div style={{ marginTop: "25px", marginLeft: "20px" }}>
                   <InputGroup>
                     <Button
                       //   onClick={handleSearchUsersAppointments}
@@ -58,7 +252,7 @@ function TableBlog() {
                       ค้นหา
                     </Button>
                   </InputGroup>
-                </div>
+                </div> */}
               </div>
               <br />
               <table className="table table-striped ">
@@ -66,6 +260,9 @@ function TableBlog() {
                   <tr className="text-center">
                     <th>
                       <h3>ลำดับ</h3>
+                    </th>
+                    <th>
+                      <h3>รูป</h3>
                     </th>
                     <th>
                       <h3>ชื่อบทความ</h3>
@@ -79,12 +276,102 @@ function TableBlog() {
                   </tr>
                 </thead>
                 <tbody>
-                  <tr>
-                    <td>test</td>
-                    <td>test</td>
-                    <td>test</td>
-                    <td>test</td>
-                  </tr>
+                  {shouldShowAllData ? (
+                    <>
+                      {displayedBlogs && displayedBlogs.length > 0 ? (
+                        displayedBlogs
+                          .slice(
+                            currentPage * perPage,
+                            (currentPage + 1) * perPage
+                          )
+                          .map((blogs, index) => (
+                            <tr key={blogs.Blog_ID} className="text-center">
+                              <td>
+                                <h3>{index + 1}</h3>
+                              </td>
+                              <td>
+                                {" "}
+                                <img
+                                  src={`${BASE_URL}/${blogs.Blog_ImageBanner}`}
+                                  alt=""
+                                  style={{ maxWidth: "300px" }}
+                                />
+                              </td>
+                              <td>
+                                <h3>{blogs.Blog_Name}</h3>
+                              </td>
+                              <td>
+                                <h3>{blogs.Blog_Detail}</h3>
+                              </td>
+                              <td>
+                                <Button
+                                  variant="primary"
+                                  onClick={() => handleEditModal(blogs.Blog_ID)}
+                                >
+                                  <h4>จัดการ</h4>
+                                </Button>{" "}
+                                <Button
+                                  variant="danger"
+                                  onClick={() => handleDelete(blogs.Blog_ID)}
+                                >
+                                  <h4>ลบ</h4>
+                                </Button>
+                              </td>
+                            </tr>
+                          ))
+                      ) : (
+                        <tr className="text-center">
+                          <td colSpan={4}>
+                            <h2>ไม่มีข้อมูล</h2>
+                          </td>
+                        </tr>
+                      )}
+                    </>
+                  ) : (
+                    <>
+                      {/* แสดงผลลัพธ์ที่ค้นหา */}
+                      {searchResult && searchResult.length > 0 ? (
+                        <>
+                          {searchResult
+                            .slice(
+                              currentPage * perPage,
+                              (currentPage + 1) * perPage
+                            )
+                            .map((blogs, index) => (
+                              <tr key={blogs.Blog_ID} className="text-center">
+                                <td>
+                                  <h3>{index + 1}</h3>
+                                </td>
+                                <td>{blogs.Blog_Image}</td>
+                                <td>
+                                  <h3>{blogs.Blog_Name}</h3>
+                                </td>
+                                <td>
+                                  <h3>{blogs.Blog_Detail}</h3>
+                                </td>
+                                <td>
+                                  <Button variant="primary">
+                                    <h4>จัดการ</h4>
+                                  </Button>{" "}
+                                  <Button
+                                    variant="danger"
+                                    onClick={() => handleDelete(blogs.Blog_ID)}
+                                  >
+                                    <h4>ลบ</h4>
+                                  </Button>
+                                </td>
+                              </tr>
+                            ))}
+                        </>
+                      ) : (
+                        <tr className="text-center">
+                          <td colSpan={4}>
+                            <h2>ไม่มีข้อมูล</h2>
+                          </td>
+                        </tr>
+                      )}
+                    </>
+                  )}
                 </tbody>
               </table>
               <ReactPaginate
@@ -112,16 +399,146 @@ function TableBlog() {
               <Modal.Header closeButton>
                 <Modal.Title>เพิ่มบทความ</Modal.Title>
               </Modal.Header>
-
               <Modal.Body>
-                <Form></Form>
+                <Form>
+                  <Form.Group>
+                    <Form.Label>
+                      <h4>ชื่อบทความ</h4>
+                    </Form.Label>
+                    <Form.Control
+                      name="Blog_Name"
+                      placeholder="ชื่อบทความ"
+                      value={blog_Name}
+                      onChange={(e) => setBlog_Name(e.target.value)}
+                    />
+                  </Form.Group>
+                  <Form.Group>
+                    <Form.Label>
+                      <h4>รายละเอียดบทความ</h4>
+                    </Form.Label>
+                    <Form.Control
+                      as="textarea"
+                      name="Blog_Detail"
+                      placeholder="รายละเอียดชื่อบทความ"
+                      value={blog_Detail}
+                      onChange={(e) => setBlog_Detail(e.target.value)}
+                    />
+                  </Form.Group>
+                  {/* ฟิลด์รูปบทความ */}
+                  <Form.Group>
+                    <Form.Label>รูปบทความ</Form.Label>
+                    <div class="custom-file">
+                      <input
+                        id="logo"
+                        type="file"
+                        class="custom-file-input"
+                        onChange={(e) =>
+                          handleImageChange(
+                            e,
+                            setPackageImage,
+                            setPackagePreview
+                          )
+                        }
+                      />
+                      <label for="logo" class="custom-file-label">
+                        Choose file...
+                      </label>
+                    </div>
+                    {packagePreview && (
+                      <div style={{ marginTop: "20px" }}>
+                        <img
+                          src={packagePreview}
+                          alt="Preview"
+                          style={{ maxWidth: "300px" }}
+                        />
+                      </div>
+                    )}
+                  </Form.Group>
+                  {/* ฟิลด์รูปภาพแบนเนอร์ */}
+                  <Form.Group>
+                    <Form.Label>รูปแบนเนอร์</Form.Label>
+                    <div class="custom-file">
+                      <input
+                        id="logo"
+                        type="file"
+                        class="custom-file-input"
+                        onChange={(e) =>
+                          handleImageChange(e, setBannerImage, setBannerPreview)
+                        }
+                      />
+                      <label for="logo" class="custom-file-label">
+                        Choose file...
+                      </label>
+                    </div>
+                    {bannerPreview && (
+                      <div style={{ marginTop: "20px" }}>
+                        <img
+                          src={bannerPreview}
+                          alt="Preview"
+                          style={{ maxWidth: "300px" }}
+                        />
+                      </div>
+                    )}
+                  </Form.Group>
+                </Form>
               </Modal.Body>
 
               <Modal.Footer>
                 <Button variant="secondary" onClick={handleClose}>
                   ยกเลิก
                 </Button>
-                <Button variant="primary">บันทึก</Button>
+                <Button variant="primary" onClick={handleSave}>
+                  บันทึก
+                </Button>
+              </Modal.Footer>
+            </Modal>
+            {/* modal จัดการบทความ */}
+            <Modal show={showEdite} onHide={handleClose}>
+              <Modal.Header closeButton>
+                <Modal.Title>จัดการบทความ</Modal.Title>
+              </Modal.Header>
+
+              <Modal.Body>
+                {selectedBlogs && (
+                  <Form>
+                    <Form.Group controlId="packageName">
+                      <Form.Label>ชื่อบทความ</Form.Label>
+                      <Form.Control
+                        type="text"
+                        placeholder="ชื่อบทความ"
+                        value={selectedBlogs.Blog_Name}
+                        onChange={(e) =>
+                          setSelectedBlogs({
+                            ...selectedBlogs,
+                            Blog_Name: e.target.value,
+                          })
+                        }
+                      />
+                    </Form.Group>
+                    <Form.Group>
+                      <Form.Label>รายละเอียดบทความ</Form.Label>
+                      <Form.Control
+                        as="textarea"
+                        placeholder="รายละเอียดบทความ"
+                        value={selectedBlogs.Blog_Detail}
+                        onChange={(e) =>
+                          setSelectedBlogs({
+                            ...selectedBlogs,
+                            Blog_Detail: e.target.value,
+                          })
+                        }
+                      />
+                    </Form.Group>
+                  </Form>
+                )}
+              </Modal.Body>
+              <Modal.Footer>
+                <Button variant="secondary" onClick={handleCloseEdite}>
+                  ยกเลิก
+                </Button>
+                <Button variant="primary" onClick={handleSubmitEdit}>
+                  บันทึก
+                </Button>
               </Modal.Footer>
             </Modal>
           </div>
